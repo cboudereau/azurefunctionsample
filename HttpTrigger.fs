ee9a68db23ff9eb44bdb5c595f65b11b8c162d6a
cboudereau <clement.boudereau@availpro.com>
2020-03-12 18:03:18 +0100

    init

---
+++ HttpTrigger.fs
@@ -0,0 +1,14 @@
+﻿namespace AzureFunctionSample
+
+open Microsoft.Azure.WebJobs
+open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.Http
+
+module HttpTrigger = 
+
+    [<FunctionName "hello">]
+    let run (([<HttpTrigger(Extensions.Http.AuthorizationLevel.Anonymous, "GET", "POST", Route="api/v1/{*path}")>] req: HttpRequest)) = 
+        "hello world"
+        |> OkObjectResult :> ActionResult 
+        |> async.Return 
+        |> Async.StartAsTask
